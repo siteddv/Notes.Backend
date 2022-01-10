9583afe0e19586c50550fbd5424578d5921bc355
Dimitrii Vinogradov <siteddv@gmail.com>
2022-01-10 18:06:12 +0600

    added get note list query

---
+++ Notes.Application/Notes/Queries/GetNoteList/GetNoteListQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Notes.Application.Notes.Queries.GetNoteList;
+using System;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails
+{
+    public class GetNoteListQuery : IRequest<NoteListVm>
+    {
+        public Guid UserId { get; set; }
+    }
+}
---
+++ Notes.Application/Notes/Queries/GetNoteList/GetNoteListQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Common.Exceptions;
+using Notes.Application.Interfaces;
+using Notes.Application.Notes.Queries.GetNoteList;
+using Notes.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails
+{
+    public class GetNoteListQueryHandler : IRequestHandler<GetNoteListQuery, NoteListVm>
+    {
+        private readonly INotesDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetNoteListQueryHandler(INotesDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<NoteListVm> Handle(GetNoteListQuery request, CancellationToken cancellationToken)
+        {
+            var notesQuery = await _dbContext.Notes
+                .Where(note => note.UserId == request.UserId)
+                .ProjectTo<NoteLookupDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            return new NoteListVm { Notes = notesQuery };
+        }
+    }
+}
