448c1eade2711ab7dbd327acc7bf7b65a3c7dcb9
Dimitrii Vinogradov <siteddv@gmail.com>
2022-01-10 18:24:23 +0600

    added extensions for DI

---
+++ Notes.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Interfaces;
+using System.Reflection;
+
+namespace Notes.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
