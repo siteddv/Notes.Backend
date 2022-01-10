88e18e216e36737c3cfdc7f63f9cfa8541304dee
Dimitrii Vinogradov <siteddv@gmail.com>
2022-01-11 02:19:30 +0600

    added get handlers to controller

---
+++ Notes.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Notes.Application.Notes.Queries.GetNoteDetails;
+using Notes.Application.Notes.Queries.GetNoteList;
+using System;
+using System.Threading.Tasks;
+
+namespace Notes.WebApi.Controllers
+{
+    public class NoteController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<NoteListVm>> GetAll()
+        {
+            var query = new GetNoteListQuery
+            {
+                UserId = UserId
+            };
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<NoteListVm>> Get(Guid id)
+        {
+            var query = new GetNoteDetailsQuery
+            {
+                UserId = UserId,
+                Id = id
+            };
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+
+        }
+    }
+}
