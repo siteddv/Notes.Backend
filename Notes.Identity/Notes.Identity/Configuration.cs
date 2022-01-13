5f8fed064b0cf3d6f2593dc8ed7e6c80b80df66d
Dimitrii Vinogradov <siteddv@gmail.com>
2022-01-14 02:38:50 +0600

    started configuring identity server

---
+++ Notes.Identity/Notes.Identity/Configuration.cs
@@ -0,0 +1,31 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Notes.Identity
+{
+    public class Configuration
+    {
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("NotesWebAPI", "Web API")
+            };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+                new ApiResource("NotesWebAPI", "Web API", new[] { JwtClaimTypes.Name })
+                {
+                    Scopes = {"NotesWebApi"}
+                }
+            };
+    }
+}
